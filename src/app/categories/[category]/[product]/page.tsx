970f844b7f78a9dc8176ad3b9761a7b8ba07035f
A-Novychenko <A.Novychenko@outlook.com>
2024-11-24 14:19:49 +0200

    fix

--- src/app/categories/[category]/[product]/page.tsx
+++ src/app/categories/[category]/[product]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { getProducts } from '@/actions/servicesAPI';
 // import { CategoryList } from '@/components/base';
 import { ProductCard } from '@/components/ui';
