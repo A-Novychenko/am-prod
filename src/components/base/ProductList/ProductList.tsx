5d7eb9431dbdb20b5eec4790d2849379b18f0b6e
A-Novychenko <A.Novychenko@outlook.com>
2024-11-25 21:52:39 +0200

    fix

--- src/components/base/ProductList/ProductList.tsx
+++ src/components/base/ProductList/ProductList.tsx
@@ -4,10 +4,10 @@ import { Product, ProductListProps } from './types';
 
 export const ProductList: React.FC<ProductListProps> = ({ products }) => {
   return (
-    <ul className="flex flex-wrap gap-6 xl:flex-row smOnly:justify-center">
+    <ul className="flex flex-col flex-wrap gap-6 xl:flex-row smOnly:justify-center">
       {products &&
         products.map((product: Product) => (
-          <li key={product._id}>
+          <li key={product._id} className="self-stretch">
             <ProductCard product={product} />
           </li>
         ))}
