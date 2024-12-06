81e63b60a2ec7f63993373b6a979df28de4458b3
A-Novychenko <A.Novychenko@outlook.com>
2024-12-06 17:21:32 +0200

    test

--- src/actions/servicesAPI/getProductsByTecDocArticle.ts
+++ src/actions/servicesAPI/getProductsByTecDocArticle.ts
@@ -1,19 +1,21 @@
-const API_URL = process.env.API_URL as string;
-
 export const getProductsByTecDocArticle = async (
   article: string,
   // page?: number,
 ) => {
   try {
-    const result = await fetch(`${API_URL}/catalog/search-products`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    // const result = await fetch(`http://localhost:3005/api/catalog/search-products`, {
+    const result = await fetch(
+      `https://autoparts-backend.onrender.com/api/catalog/search-products`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        next: { revalidate: 0 },
+        // cache: 'no-cache',
+        body: JSON.stringify({ article }),
       },
-      next: { revalidate: 0 },
-      // cache: 'no-cache',
-      body: JSON.stringify({ article }),
-    });
+    );
 
     const res = await result.json();
 
