2ce9361108ffdcf5830cea210c5daa9427e634c6
A-Novychenko <A.Novychenko@outlook.com>
2024-12-06 17:08:46 +0200

    fix path

--- src/actions/servicesAPI/getProductsByTecDocArticle.ts
+++ src/actions/servicesAPI/getProductsByTecDocArticle.ts
@@ -5,20 +5,15 @@ export const getProductsByTecDocArticle = async (
   // page?: number,
 ) => {
   try {
-    const result = await fetch(
-      // `${API_URL}/catalog/products?id=${id}&page=${page}`,
-      // `${API_URL}/catalog/search-products`,
-      `http://localhost:3005/api/catalog/search-products`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        next: { revalidate: 0 },
-        // cache: 'no-cache',
-        body: JSON.stringify({ article }),
+    const result = await fetch(`{API_URL}/api/catalog/search-products`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      next: { revalidate: 0 },
+      // cache: 'no-cache',
+      body: JSON.stringify({ article }),
+    });
 
     const res = await result.json();
 
