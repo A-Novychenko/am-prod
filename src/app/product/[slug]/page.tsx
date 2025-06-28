9c594048270c8386b8b972709526c78f0250e633
A-Novychenko <A.Novychenko@outlook.com>
2025-06-28 17:46:10 +0300

    fix and redeploy

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,5 @@
 export const revalidate = 3600;
 
-import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import { CastrolSeoSection, SingleProductSection } from '@/sections';
@@ -54,35 +53,14 @@ export default async function SingleProductPage({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/product/${slug}`,
   });
 
-  if (!product) {
-    return (
-      <>
-        <script
-          type="application/ld+json"
-          suppressHydrationWarning
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-        />
-
-        <section className="flex min-h-[50vh] items-center justify-center bg-gray-50 px-4">
-          <div className="text-center">
-            <h1 className="mb-4 text-3xl font-bold text-gray-800">
-              Товар не знайдено
-            </h1>
-            <p className="text-gray-500">
-              Спробуйте перевірити правильність посилання або поверніться на{' '}
-              <Link href="/" className="text-blue-700">
-                головну сторінку
-              </Link>
-              .
-            </p>
-          </div>
-        </section>
-      </>
-    );
-  }
-
   return (
     <>
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+      />
+
       <h1 className="visually-hidden">{product.name}</h1>
 
       <SingleProductSection product={product} />
